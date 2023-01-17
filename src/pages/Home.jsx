e345ed8b766508fd2592ba3b4467df85d05f086c
T31K <t31kmunwong@gmail.com>
2023-01-17 16:09:21 +0800

    feat(initial): initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,30 +7,20 @@ import FeaturesBlocks from '../partials/FeaturesBlocks';
 import Testimonials from '../partials/Testimonials';
 import Newsletter from '../partials/Newsletter';
 import Footer from '../partials/Footer';
-import Banner from '../partials/Banner';
 
 function Home() {
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
-
-      {/*  Site header */}
       <Header />
-
-      {/*  Page content */}
+      
       <main className="flex-grow">
-
-        {/*  Page sections */}
         <HeroHome />
         <FeaturesHome />
         <FeaturesBlocks />
         <Testimonials />
         <Newsletter />
-
       </main>
-
-      <Banner />
-
-      {/*  Site footer */}
+      
       <Footer />
 
     </div>
