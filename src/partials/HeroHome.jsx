61f9783f25b3978a4882aaeb12d778aac75be026
T31K <t31kmunwong@gmail.com>
2023-01-28 21:47:25 +0800

    auto-commit

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -77,7 +77,7 @@ function HeroHome() {
                 </svg>
                 <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 font-semibold rounded-xl w-full mb-4 sm:w-auto sm:mb-0" href="#0">Download Free</a>
+                  <a className="btn text-white bg-purple-600 hover:bg-blue-700 font-semibold rounded-xl w-full mb-4 sm:w-auto sm:mb-0" href="#0">Download Free</a>
                 </div>
                 <div>
                   <a className="btn text-white bg-gray-800 hover:bg-gray-800 font-semibold rounded-xl w-full sm:w-auto sm:ml-4" href="#0">Purchase License</a>
