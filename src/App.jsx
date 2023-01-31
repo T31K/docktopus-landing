f21265453dfca22ab1c43cf4a6d5009a9ebb6378
T31K <t31kmunwong@gmail.com>
2023-01-31 14:20:54 +0800

    autoupdate

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React, { useEffect } from 'react';
-import {
-  Routes,
-  Route,
-  useLocation
-} from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import 'aos/dist/aos.css';
 import './css/style.css';
@@ -15,8 +11,10 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ResetPassword from './pages/ResetPassword';
 
-function App() {
+import { EmojiProvider } from 'react-apple-emojis';
+import emojiData from 'react-apple-emojis/src/data.json';
 
+function App() {
   const location = useLocation();
 
   useEffect(() => {
@@ -29,19 +27,34 @@ function App() {
   });
 
   useEffect(() => {
-    document.querySelector('html').style.scrollBehavior = 'auto'
-    window.scroll({ top: 0 })
-    document.querySelector('html').style.scrollBehavior = ''
+    document.querySelector('html').style.scrollBehavior = 'auto';
+    window.scroll({ top: 0 });
+    document.querySelector('html').style.scrollBehavior = '';
   }, [location.pathname]); // triggered on route change
 
   return (
     <>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-      </Routes>
+      <EmojiProvider data={emojiData}>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/signin"
+            element={<SignIn />}
+          />
+          <Route
+            path="/signup"
+            element={<SignUp />}
+          />
+          <Route
+            path="/reset-password"
+            element={<ResetPassword />}
+          />
+        </Routes>
+      </EmojiProvider>
     </>
   );
 }
